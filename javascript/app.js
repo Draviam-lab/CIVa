3fee2194aa6a438ad01ff2d1493137832418efc0
TruptiG <t.gore@mail.cryst.bbk.ac.uk>
2022-01-04 15:27:34 +0000

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -45,7 +45,7 @@ d3.csv("data/civ_data.csv").then(function (data)
             
             var filteredChromsGene = civ.filter(civ => civ.Gene_Symbol === inputValueGene); // checks datatype
             
-            if (filteredChromsGene.length === 0 && inputValueGene !="") {
+            if (filteredChromsGene.length === 0 && inputValueGene !=="") {
                 d3.select("p").classed('noresults', true).html("<strong>No record to match this gene symbol. Please contact to add this to the database</strong>")
             }
             for (var k = 0; k < filteredChromsGene.length; k++) {
@@ -72,7 +72,7 @@ d3.csv("data/civ_data.csv").then(function (data)
             
             var filteredUniprot = civ.filter(civ => civ.Uniprot_ID===inputValueUniprot); // checks datatype
            
-            if (filteredUniprot.length === 0 && inputValueUniprot !=""){
+            if (filteredUniprot.length === 0 && inputValueUniprot !==""){
                 d3.select("p").classed('noresults', true).html("<strong>No record to match this uniprot id. Please contact to add this to the database</strong>")
             }
             for (var i = 0; i < filteredUniprot.length; i++) {
